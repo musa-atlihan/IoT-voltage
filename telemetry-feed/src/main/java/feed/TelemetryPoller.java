4a77b44f251fb467724577661a5d831ff87664de
musa-atlihan <msatlihan@gmail.com>
2020-11-05 23:03:24 +0300

    add telemetry runnable and producer runnable

--- telemetry-feed/src/main/java/feed/TelemetryPoller.java
+++ telemetry-feed/src/main/java/feed/TelemetryPoller.java
@@ -1,11 +1,18 @@
 package feed;
 
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.Random;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
 
 public class TelemetryPoller {
     public TelemetryPoller() {
@@ -29,4 +36,109 @@ public class TelemetryPoller {
         // shutdown hook
 
     }
+
+    public class ProducerRunnable implements Runnable {
+
+        Logger logger = LoggerFactory.getLogger(ProducerRunnable.class);
+        KafkaProducer<String, String> producer;
+        BlockingQueue<SensorReading> queue;
+        String topic;
+
+        public ProducerRunnable(String propsPath, BlockingQueue<SensorReading> queue) {
+
+            try {
+                this.queue= queue;
+
+                FileInputStream in = new FileInputStream(propsPath);
+
+                Properties properties = new Properties();
+                properties.load(in);
+                this.topic = properties.getProperty("topic");
+                in.close();
+
+                producer = new KafkaProducer<String, String>(properties);
+            } catch (IOException e) {
+                logger.error("Kafka Producer properties file read failure!");
+            }
+
+        }
+
+        @Override
+        public void run() {
+
+            while (true) {
+                try {
+                    SensorReading reading = queue.poll(5, TimeUnit.SECONDS);
+                    logger.info("Producer message read " + reading.toString());
+                    producer.send(new ProducerRecord<String, String>(topic, reading.toString()));
+                } catch (InterruptedException e) {
+                    logger.info("Producer interrupted! " + e);
+                }
+            }
+        }
+
+        public void shutdown() {
+            producer.close();
+            logger.info("Kafka Producer has closed.");
+        }
+    }
+
+    public class TelemetryRunnable implements Runnable {
+
+        Logger logger = LoggerFactory.getLogger(TelemetryRunnable.class);
+        private BlockingQueue<SensorReading> queue;
+
+        public TelemetryRunnable(BlockingQueue<SensorReading> queue) {
+            this.queue = queue;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+
+                    SensorReading reading = new SensorReading();
+                    reading.setMachineId(produceMachineId());
+                    reading.setVoltage(produceSensorReading());
+
+                    boolean success = queue.offer(reading, 100, TimeUnit.MILLISECONDS);
+                }
+            } catch (InterruptedException e) {
+                logger.info("Telemetry runnable interrupted!");
+            }
+
+        }
+
+        private Integer produceMachineId() {
+            Random random = new Random();
+            Integer number = random.nextInt(100);
+
+            // simulate reading time
+            try {
+                Thread.sleep(random.nextInt(1000));
+            } catch (InterruptedException e) {
+                logger.info("MachineId generation interrupted!");
+            }
+            return number;
+        }
+
+        private Long produceSensorReading() {
+            Random random = new Random();
+            long leftLimit = 800L;
+            long rightLimit = 1500L;
+
+            long number = leftLimit + (long) (Math.random() * (rightLimit - leftLimit));
+
+            // simulate reading time
+            try {
+                Thread.sleep(random.nextInt(1000));
+            } catch (InterruptedException e) {
+                logger.info("SensorReading generation interrupted!");
+            }
+            return number;
+        }
+
+
+    }
+
 }

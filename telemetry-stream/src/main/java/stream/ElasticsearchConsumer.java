50e281986def45c88aeca6a9c216503c0d777af5
musa-atlihan <msatlihan@gmail.com>
2020-11-06 15:15:36 +0300

    create consumer and run

--- telemetry-stream/src/main/java/stream/ElasticsearchConsumer.java
+++ telemetry-stream/src/main/java/stream/ElasticsearchConsumer.java
@@ -1,15 +1,23 @@
 package stream;
 
 import org.apache.http.HttpHost;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
 
@@ -23,6 +31,35 @@ public class ElasticsearchConsumer {
 
         // create kafka consumer
         KafkaConsumer<String, String> consumer = createConsumer();
+
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+            BulkRequest bulkRequest = new BulkRequest();
+
+            for (ConsumerRecord<String, String> record : records) {
+                // kafka generic id to prevent document duplication
+                String id = record.topic() + "_" + record.partition() + '_' + record.offset();
+                logger.info("Consumer reads: " + record.value());
+
+                try {
+                    // insert data into elasticsearch
+                    IndexRequest indexRequest = new IndexRequest("machine-telemetry")
+                            .id(id)
+                            .source(record.value(), XContentType.JSON);
+
+                    bulkRequest.add(indexRequest);
+                } catch (NullPointerException e) { // skip bad data
+                    logger.warn("Skipping bad data: " + record.value());
+                }
+            }
+
+            if (records.count() > 0) {
+                BulkResponse bulkItemResponses = client.bulk(bulkRequest, RequestOptions.DEFAULT);
+                consumer.commitSync();
+            }
+        }
 
     }
 
